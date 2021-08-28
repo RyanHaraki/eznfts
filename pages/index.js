a8478c1ab085947a2fbd920e6aaa8bcd46cb846a
Ryan Haraki <ryan.haraki@hotmail.com>
2021-08-28 04:10:21 -0700

    POST request updates

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DropZone from "../components/dropzone";
 import { uploadNFT } from "../lib/apiCalls";
 
 export default function Home() {
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState({});
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [wallet, setWallet] = useState("");
   const [preview, setPreview] = useState({});
 
+  useEffect(() => {
+    uploadNFT(
+      "bob",
+      "text",
+      "0x1928a49e355229281762e10511A3138a68E77fE8",
+      file
+    );
+  });
+
   // upload file and add it to state
   const onFileChange = (e) => {
-    const newFile = e.target.files[0];
-    console.log(`file: ${newFile}`);
-    // BUG: not working
-    setFile(newFile);
-    console.log(file);
-    setPreview(URL.createObjectURL(newFile));
+    setFile(e.target.files[0]);
+    //set the preview
+    setPreview(URL.createObjectURL(e.target.files[0]));
   };
 
   // change handlers for input fields
@@ -26,7 +32,10 @@ export default function Home() {
 
   return (
     <div>
-      <form method="POST">
+      <form
+        method="POST"
+        onSubmit={() => uploadNFT(name, description, wallet, file)}
+      >
         {/* Inputs */}
         <input
           type="text"
@@ -53,12 +62,7 @@ export default function Home() {
 
         {/* Image and upload button */}
         <img src={preview} />
-        <button
-          type="submit"
-          onClick={() => uploadNFT(name, description, wallet, file)}
-        >
-          Upload
-        </button>
+        <button type="submit">Upload</button>
       </form>
     </div>
   );
